1ba56da28668014f02262982dfd5c7ab309d9982
Danielle Chard <42789596+Daniellechard@users.noreply.github.com>
2018-10-29 11:54:57 -0600

    working random words and mouse clicks

--- hw-10/empty-example/sketch.js
+++ hw-10/empty-example/sketch.js
@@ -11,7 +11,8 @@ var index=0;
 //setting up canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+//slowing frame rate
+frameRate(1);
   }
 
 
@@ -19,12 +20,16 @@ function setup() {
 function draw(){
 background(235);
 
+//adding random word positions for x and y
+pos.x = random(0, width);
+pos.y = random(0, height);
+
 //creating text
 fill(0);
 textSize(50);
 text(words[index], pos.x, pos.y);}
 
-//changing text with each mouse clickS
+//changing text with each mouse click and reseting it once it reaches the last word
 function mousePressed() {
   index = index + 1;
 
