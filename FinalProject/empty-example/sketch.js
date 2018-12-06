df2fddc7da37c62a73662caf7a9fb42922eda316
Danielle Chard <42789596+Daniellechard@users.noreply.github.com>
2018-12-06 12:11:30 -0700

    working getEnergy functions

--- FinalProject/empty-example/sketch.js
+++ FinalProject/empty-example/sketch.js
@@ -14,10 +14,38 @@ function preload(){
 
 function setup() {
   mySong.play();
+  //stores song's FFT data
   fft = new p5.FFT();
-
+    createCanvas(windowWidth, windowHeight);
+//slowing frame rate
+frameRate(3);
 }
 
 function draw() {
   background(200);
+  //analyzes frequencies and pulls them from data
+  fft.analyze();
+
+
+
+//creating variables for frequencies and finding different frequency ranges
+  var bass = fft.getEnergy("bass");
+  var lowMid = fft.getEnergy("lowMid");
+  var mid = fft.getEnergy("mid");
+  var highMid = fft.getEnergy("highMid");
+  var treble = fft.getEnergy("treble");
+
+//creating text to be able to see frequencies as song plays
+  push();
+  
+text(bass, 10, 10,20,20);
+text(lowMid, 10, 20, 20, 20);
+text(mid, 10, 30, 20, 20);
+text(highMid, 10, 40, 20, 20);
+text(treble, 10, 50, 20, 20);
+
+  pop();
+
+//mapping frequencies
+  bass
 }
